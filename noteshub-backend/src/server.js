04c5cf1a022c7ae95c58ada727971a800aab39d1
Lakshmi Vara prasad <lakshmivaraprasadmaddiboina@gmail.com>
2025-08-31 23:04:55 +0530

    removed the backend folder

--- noteshub-backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-require("dotenv").config();
-const connectDB = require("./config/db");
-connectDB(process.env.MONGO_URI);
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
