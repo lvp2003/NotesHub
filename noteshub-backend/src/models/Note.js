fb7e6c1204699ca42dd295c200cd9cfa84673036
Lakshmi Vara prasad <lakshmivaraprasadmaddiboina@gmail.com>
2025-08-31 23:32:42 +0530

    database user and notes are added

---
+++ noteshub-backend/src/models/Note.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
