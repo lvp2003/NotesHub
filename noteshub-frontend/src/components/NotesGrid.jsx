f0905a7d7d1e999792eae3307ecf49b9b849fea7
Lakshmi Vara prasad <lakshmivaraprasadmaddiboina@gmail.com>
2025-08-31 11:17:37 +0530

    updated the serachfilter

--- noteshub-frontend/src/components/NotesGrid.jsx
+++ noteshub-frontend/src/components/NotesGrid.jsx
@@ -16,42 +16,83 @@ const NotesGrid = () => {
       subject: "DBMS",
       uploadedBy: "Charlie",
     },
+    {
+      id: 1,
+      title: "Data Structures Notes",
+      subject: "DSA",
+      uploadedBy: "Alice",
+    },
+    { id: 2, title: "Operating Systems", subject: "OS", uploadedBy: "Bob" },
+    {
+      id: 3,
+      title: "Database Management",
+      subject: "DBMS",
+      uploadedBy: "Charlie",
+    },
+    {
+      id: 1,
+      title: "Data Structures Notes",
+      subject: "DSA",
+      uploadedBy: "Alice",
+    },
+    { id: 2, title: "Operating Systems", subject: "OS", uploadedBy: "Bob" },
+    {
+      id: 3,
+      title: "Database Management",
+      subject: "DBMS",
+      uploadedBy: "Charlie",
+    },
+    {
+      id: 1,
+      title: "Data Structures Notes",
+      subject: "DSA",
+      uploadedBy: "Alice",
+    },
+    { id: 2, title: "Operating Systems", subject: "OS", uploadedBy: "Bob" },
+    {
+      id: 3,
+      title: "Database Management",
+      subject: "DBMS",
+      uploadedBy: "Charlie",
+    },
   ];
-
+  const filterednotes = notes.filter(
+    (note) =>
+      note.title.toLowerCase().includes(query.toLowerCase()) ||
+      note.subject.toLowerCase().includes(query.toLowerCase())
+  );
   return (
     <section className="py-12 bg-gray-50">
       <div className="max-w-6xl mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-6">Browse Notes</h2>
         <input
           type="text"
-          placeholder="Search notes by title or subject..."
+          placeholder="Search notes by title or subject"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           className="w-full sm:w-2/3 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pt-5">
-          {notes.length > 0 ? (
-            notes
-              .filter(
-                (note) =>
-                  note.title.toLowerCase().includes(query.toLowerCase()) ||
-                  note.subject.toLowerCase().includes(query.toLowerCase())
-              )
-              .map((note) => (
-                <div
-                  key={note.id}
-                  className="bg-white shadow-lg rounded-xl p-6 hover:shadow-xl transition"
-                >
-                  <h3 className="text-xl font-semibold mb-2">{note.title}</h3>
-                  <p className="text-gray-600">📘 {note.subject}</p>
-                  <p className="text-gray-500 text-sm">
-                    Uploaded by {note.uploadedBy}
-                  </p>
-                  <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
-                    Download
-                  </button>
-                </div>
-              ))
+          {filterednotes.length > 0 ? (
+            filterednotes.map((note) => (
+              <div
+                key={note.id}
+                className="bg-gray-900 shadow-lg rounded-xl p-6 hover:shadow-xl transition text-white"
+              >
+                <h3 className="text-xl font-bold mb-2 text-white">
+                  {note.title}
+                </h3>
+                <p className="text-gray-600 font-semibold text-white">
+                  📘 {note.subject}
+                </p>
+                <p className="text-gray-500 text-sm text-white">
+                  Uploaded by {note.uploadedBy}
+                </p>
+                <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+                  Download
+                </button>
+              </div>
+            ))
           ) : (
             <p className="text-gray-500">No notes found.</p>
           )}
