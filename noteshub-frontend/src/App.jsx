82ffcbaa067665bc5c4f0f88aca2acf5ff664e5d
Lakshmi Vara prasad <lakshmivaraprasadmaddiboina@gmail.com>
2025-09-01 17:56:48 +0530

    going with firebase database to store the data

--- noteshub-frontend/src/App.jsx
+++ noteshub-frontend/src/App.jsx
@@ -4,14 +4,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import NotesGrid from "./components/NotesGrid";
 import PageNotFound from "./components/PageNotFound";
-// import UploadPage from "./components/UploadPage";
-// import BrowseNotes from "./components/BrowseNotes";
-// import PdfViewer from "./components/PdfViewer";
-
 const App = () => {
   const UploadPage = lazy(() => import("./components/UploadPage"));
-  const BrowseNotes = lazy(() => import("./components/BrowseNotes"));
-  const PdfViewer = lazy(() => import("./components/PdfViewer"));
   return (
     <Router>
       <Navbar />
